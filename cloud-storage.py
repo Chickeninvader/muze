1b28f9cc8f31dc22352ef3761e0b97142583890e
dnguye92asu <80861888+dnguye92asu@users.noreply.github.com>
2023-11-11 08:47:25 -0700

    decode operation

--- cloud-storage.py
+++ cloud-storage.py
@@ -7,14 +7,11 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "cloud-key.json"
 # Set up the client
 client = storage.Client()
 
-# Set the name of the bucket and the local folder to upload
 bucket_name = "muze-train-data"
-folder_name = "audio/"
+folder_name = "decoded/"
 
 # Get the bucket
 bucket = client.get_bucket(bucket_name)
-
-# Upload the folder to the bucket
 
 # TODO: Cloud storage operations
 
